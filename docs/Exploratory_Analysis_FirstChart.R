f01b526231f10436406fdc6035e0ed4e9d890547
jaydenset <jaydenset@gmail.com>
2022-02-19 00:22:48 -0800

    added line chart

---
+++ docs/Exploratory_Analysis_FirstChart.R
@@ -0,0 +1,12 @@
+library("tidyverse")
+library("ggplot2")
+
+dataset <- read.csv("https://raw.githubusercontent.com/info-201a-wi22/final-project-starter-info/main/data/covid-vaccination-vs-death_ratio.csv")
+
+dataset %>%
+  filter(country == "United States of America") %>%
+  ggplot(aes(x = date, y = total_vaccinations, colour = country))+
+  geom_point(size = 5, alpha = 0.2)+
+  geom_line(size = 1)+
+  theme_minimal()+
+  labs(title = "Progession of Vaccinations in the US")
