daccd2143a9060cb69e46f8d7fdceb76e4e3ca85
Emma Bacarra <ebacarra@uw.edu>
2022-02-18 19:49:07 -0800

    Update

--- docs/Exploratory_Analysis_SecondChart.R
+++ docs/Exploratory_Analysis_SecondChart.R
@@ -4,28 +4,31 @@ data <- read.csv("https://data.cdc.gov/api/views/8xkx-amqh/rows.csv?accessType=D
 View(data)
 
 colnames(data)[1] <- "Date"
+data$date <- as.Date(data$Date, "%m/%d/%Y")
+data$date
+class(data$date)
 
 recentdata <-
   data %>%
-  filter(date == max(date)) %>%
+  filter(date == (max(date))) %>%
   summarize( 
-    date,
-    recip_county,
-    recip_state,
-    Census2019, #census population
-    Administered_Dose1_Pop_Pct, #% w/ at least 1 dose by state
-    Census2019_5PlusPop, #census population age group 5
-    Administered_Dose1_Recip_5PlusPop_Pct, #% at least 1 dose age group 5
-    Census2019_12PlusPop, #census population age group 12
-    Administered_Dose1_Recip_12PlusPop_Pct, #% at least 1 dose age group 12
-    Census2019_18PlusPop, #census population age group 18
-    Administered_Dose1_Recip_18PlusPop_Pct, #% at least 1 dose age group 18
-    Census2019_65PlusPop, #census population age group 65
-    Administered_Dose1_Recip_65PlusPop_Pct #% at least 1 dose age group 65
+    Date,
+    Recip_County,
+    Recip_State,
+    Census2019, # census population
+    Administered_Dose1_Pop_Pct, # % w/ at least 1 dose by state
+    Census2019_5PlusPop, # census population age group 5
+    Administered_Dose1_Recip_5PlusPop_Pct, # % at least 1 dose age group 5
+    Census2019_12PlusPop, # census population age group 12
+    Administered_Dose1_Recip_12PlusPop_Pct, # % at least 1 dose age group 12
+    Census2019_18PlusPop, # census population age group 18
+    Administered_Dose1_Recip_18PlusPop_Pct, # % at least 1 dose age group 18
+    Census2019_65PlusPop, # census population age group 65
+    Administered_Dose1_Recip_65PlusPop_Pct # % at least 1 dose age group 65
   ) %>%
   rename(
-    county = recip_county,
-    state = recip_state,
+    county = Recip_County,
+    state = Recip_State,
     totpop_2019 = Census2019,
     state_vax_pct = Administered_Dose1_Pop_Pct,
     age5_pop2019 = Census2019_5PlusPop,
@@ -38,4 +41,9 @@ recentdata <-
     age65_pctvax = Administered_Dose1_Recip_65PlusPop_Pct
   )
 View(recentdata)
+
+# `recentdata` compares population and percentage of that population
+# with at least 1 dose of the vaccine. Includes general population 
+# (i.e. consensus population) and general vaccinated percentage, along
+# with statistics by age groups: 5+, 12+, 18+, and 65+. 
   
