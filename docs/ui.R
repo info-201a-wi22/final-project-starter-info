e9dee31d32fbed6200e988da04c5e9b9ed423fca
Emma Bacarra <ebacarra@uw.edu>
2022-03-06 14:34:57 -0800

    Updating

--- docs/ui.R
+++ docs/ui.R
@@ -11,21 +11,42 @@ introductory_page <- tabPanel("Introduction",
 
 interactive_page_1 <- tabPanel(
   
+  # Three interactive pages.  Each of these pages will consider a different aspect of 
+  # your data, addressing specific questions. Each page should have sufficient 
+  # interactivity (e.g., Shiny widgets + 1 or more reactive charts) for exploring the 
+  # question of interest. If you would prefer to make fewer pages with more complexity, 
+  # check with your TA/professor. The key goal: Present visualizations that help your 
+  # audience understand your datasets.
+  
+  # ^^ I talked to Samuel & he said we only need to do two because of our situation
 )
 
 
-interactive_page_2 <- tabPanel(""
+interactive_page_2 <- tabPanel(
+  
+  # Three interactive pages.  Each of these pages will consider a different aspect of 
+  # your data, addressing specific questions. Each page should have sufficient 
+  # interactivity (e.g., Shiny widgets + 1 or more reactive charts) for exploring the 
+  # question of interest. If you would prefer to make fewer pages with more complexity, 
+  # check with your TA/professor. The key goal: Present visualizations that help your 
+  # audience understand your datasets.
   
 )
 
 
 summary_page <- tabPanel("Summary",
-  
+  # Summary takeaways, a page that hones in on at least 3 major takeaways from the 
+  # project (which should be related to a specific aspect of your analysis). Feel free 
+  # to incorporate tables, graphics, or other elements to convey these conclusions. 
+  # The key goal: Present the key takeaways for your audience.
 )
 
 
 report_page <- tabPanel("Report",
+  # Report page.  Iterate on your P01 and P02 to present your final report.  See 
+  # below.  The key goal: Write a concise, clear, and interesting summary of your project. 
   
+  # Put index.rmd file here
 )
 
 
