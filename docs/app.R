c88b148b6fcdf3d5fcb94d8d1b5cf9bb3cc1924b
Emma Bacarra <ebacarra@uw.edu>
2022-03-06 14:20:36 -0800

    Updating

---
+++ docs/app.R
@@ -0,0 +1,11 @@
+library(pacman) # <- for loading multiple libraries at once
+
+p_load(
+  rsconnect, dplyr, ggplot2, shiny, reshape2, tidyverse,
+  data.table, mapdata, ggmap, maps, viridis, DT, plotly
+)
+
+source("server.R")
+source("ui.R")
+
+shinyApp(ui, server)
