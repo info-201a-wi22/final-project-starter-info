417f8c39fb4d9edbe0ca9a99b252177c1ba51563
Emma Bacarra <ebacarra@uw.edu>
2022-03-06 14:18:03 -0800

    Updating

--- docs/Exploratory_Analysis_SecondChart.R
+++ docs/Exploratory_Analysis_SecondChart.R
@@ -14,32 +14,18 @@ recentdata <-
   filter(date == (max(date))) %>%
   summarize( 
     Date,
-    Recip_County,
-    Recip_State,
-    Census2019, # census population
-    Administered_Dose1_Pop_Pct, # % w/ at least 1 dose by state
-    Census2019_5PlusPop, # census population age group 5
-    Administered_Dose1_Recip_5PlusPop_Pct, # % at least 1 dose age group 5
-    Census2019_12PlusPop, # census population age group 12
-    Administered_Dose1_Recip_12PlusPop_Pct, # % at least 1 dose age group 12
-    Census2019_18PlusPop, # census population age group 18
-    Administered_Dose1_Recip_18PlusPop_Pct, # % at least 1 dose age group 18
-    Census2019_65PlusPop, # census population age group 65
-    Administered_Dose1_Recip_65PlusPop_Pct # % at least 1 dose age group 65
-  ) %>%
-  rename(
     county = Recip_County,
     state = Recip_State,
-    totpop_2019 = Census2019,
-    state_vax_pct = Administered_Dose1_Pop_Pct,
-    age5_pop2019 = Census2019_5PlusPop,
-    age5_pctvax = Administered_Dose1_Recip_5PlusPop_Pct,
-    age12_pop2019 = Census2019_12PlusPop,
-    age12_pctvax = Administered_Dose1_Recip_12PlusPop_Pct,
-    age18_pop2019 = Census2019_18PlusPop,
-    age18_pctvax = Administered_Dose1_Recip_18PlusPop_Pct,
-    age65_pop2019 = Census2019_65PlusPop,
-    age65_pctvax = Administered_Dose1_Recip_65PlusPop_Pct
+    totpop_2019 = Census2019, # census population
+    state_vax_pct = Administered_Dose1_Pop_Pct, # % w/ at least 1 dose by state
+    age5_pop2019 = Census2019_5PlusPop, # census population age group 5
+    age5_pctvax = Administered_Dose1_Recip_5PlusPop_Pct, # % at least 1 dose age group 5
+    age12_pop2019 = Census2019_12PlusPop, # census population age group 12
+    age12_pctvax = Administered_Dose1_Recip_12PlusPop_Pct, # % at least 1 dose age group 12
+    age18_pop2019 = Census2019_18PlusPop, # census population age group 18
+    age18_pctvax = Administered_Dose1_Recip_18PlusPop_Pct, # % at least 1 dose age group 18
+    age65_pop2019 = Census2019_65PlusPop, # census population age group 65
+    age65_pctvax = Administered_Dose1_Recip_65PlusPop_Pct # % at least 1 dose age group 65
   )
 View(recentdata)
 
---
+++ docs/server.R
@@ -0,0 +1,1 @@
+# setwd("C:/Users/ebaca/Desktop/School/final-project-starter-info/docs")
