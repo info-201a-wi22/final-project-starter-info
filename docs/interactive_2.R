46eaba0371908733803d41c5ed05b4255f6c6628
Emma Bacarra <ebacarra@uw.edu>
2022-03-07 20:10:11 -0800

    Updating

--- docs/interactive_2.R
+++ docs/interactive_2.R
@@ -3,6 +3,18 @@ data <- read.csv("https://data.cdc.gov/api/views/unsk-b7fc/rows.csv?accessType=D
 colnames(data)[1] <- "Date"
 data$date <- as.Date(data$Date, "mm/dd/yyyy")
 
+state_coords <- 
+  map_data("state") %>%
+  unite(polyname, region) %>%
+  left_join(state.fips, by = "polyname") %>%
+  rename(name = polyname, State = abb)
+state_coords[state_coords$name == "massachusetts", "State"] <- "MA"
+state_coords[state_coords$name == "michigan", "State"] <- "MI"
+state_coords[state_coords$name == "north carolina", "State"] <- "NC"
+state_coords[state_coords$name == "new york", "State"] <- "NY"
+state_coords[state_coords$name == "virginia", "State"] <- "VA"
+state_coords[state_coords$name == "washington", "State"] <- "WA"
+
 General_Population <- 
   data %>%
   filter(! Location %in% c(
@@ -11,22 +23,11 @@ General_Population <-
   summarize(
     Date,
     State = Location, 
-    `Total Distributed` = as.numeric(str_replace(Distributed, ",", "")),
-    `Total Administered` = as.numeric(str_replace(Administered, ",", "")),
-    `Single Dose` = as.numeric(str_replace(Administered_Dose1_Recip, ",", "")),
-    `Full Vaccination` = as.numeric(str_replace(Series_Complete_Yes, ",", "")),
-    Booster = as.numeric(str_replace(Additional_Doses, ",", ""))
-  )
-
-state_coords <- 
-  map_data("state") %>%
-  unite(polyname, region) %>%
-  left_join(state.fips, by = "polyname") %>%
-  rename(name = polyname, State = abb)
-
-General_Population <-
-  state_coords %>%
-  left_join(General_Population, by = "State") 
-
-
+    `Total Distributed` = as.numeric(gsub(",", "", Distributed)),
+    `Total Administered` = as.numeric(gsub(",", "", Administered)),
+    `Single Dose` = as.numeric(gsub(",", "", Administered_Dose1_Recip)),
+    `Full Vaccination` = as.numeric(gsub(",", "", Series_Complete_Yes)),
+    Booster = as.numeric(gsub(",", "", Additional_Doses)),
+  ) %>%
+  right_join(state_coords, by = "State")
 
